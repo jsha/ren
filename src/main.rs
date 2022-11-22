5405d80cd2181d9cd8242a48ecfadba1f3335284
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2022-11-22 13:33:21 -0800

    Initial commit: talk to DB

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+#[derive(sqlx::FromRow, Debug)]
+struct Issuance {
+    #[allow(dead_code)]
+    name: String,
+    issuances: Vec<u8>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    env_logger::init();
+    let pool = PgPoolOptions::new()
+        .max_connections(50)
+        .connect(&std::env::var("DSN").expect("didn't find $DSN environment variable"))
+        .await?;
+
+    let name = std::env::args().nth(1).unwrap();
+    let val: i16 = std::env::args().nth(2).unwrap().parse().unwrap();
+    add_issuance(&pool, &name, val).await?;
+
+    Ok(())
+}
+
+async fn add_issuance(pool: &Pool<Postgres>, name: &str, n: i16) -> Result<(), sqlx::Error> {
+    // let tx = pool.begin();
+    let issuance = sqlx::query_as::<_, Issuance>("SELECT * FROM issuance WHERE name = $1")
+        .bind(name)
+        .fetch_optional(pool)
+        .await?;
+
+    match issuance {
+        Some(mut issuance) => {
+            issuance
+                .issuances
+                .extend_from_slice(n.to_be_bytes().as_ref());
+            sqlx::query("UPDATE issuance SET issuances = $1 WHERE name = $2")
+                .bind(issuance.issuances)
+                .bind(name)
+                .execute(pool)
+                .await?;
+
+            Ok(())
+        }
+        None => {
+            sqlx::query("INSERT INTO issuance (name, issuances) VALUES ($1, $2)")
+                .bind(name)
+                .bind(&n.to_be_bytes())
+                .execute(pool)
+                .await?;
+            Ok(())
+        }
+    }
+}
